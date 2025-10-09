d8202aa4326869f90ef17618fa2b6fb639d3c681
abidmuin <muin.739@gmail.com>
2025-10-09 13:30:38 -0300

    added TCL file

---
+++ assignment-2/a2.tcl
@@ -0,0 +1,15 @@
+# Node setup
+set n0 [$ns node]
+set n1 [$ns node]
+set n2 [$ns node]
+set n3 [$ns node]
+set n4 [$ns node]
+set n5 [$ns node]
+set n6 [$ns node]
+set n7 [$ns node]
+set n8 [$ns node]
+set n9 [$ns node]
+set n10 [$ns node]
+set n11 [$ns node]
+
+
